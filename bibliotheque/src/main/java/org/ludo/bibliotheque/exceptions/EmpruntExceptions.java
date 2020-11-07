e5350b420010ce042d45a47f8a782e37e45dfba3
ludovic <ludovic>
2020-11-07 17:44:32 +0100

    Ajout de la notion de nombre d'exemplaire pour l'objet Livre

--- bibliotheque/src/main/java/org/ludo/bibliotheque/exceptions/EmpruntExceptions.java
+++ bibliotheque/src/main/java/org/ludo/bibliotheque/exceptions/EmpruntExceptions.java
@@ -1,5 +1,7 @@
 package org.ludo.bibliotheque.exceptions;
 
+
+//TODO ajouter une classe d'exception pour les emprunts >> FAIT
 public class EmpruntExceptions extends Exception {
 
     public EmpruntExceptions(String s){
