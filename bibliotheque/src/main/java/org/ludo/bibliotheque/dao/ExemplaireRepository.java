1ca8fcdb4349557752a3df4320d11fdd4b0562e3
ludovic <ludovic>
2020-11-08 13:43:42 +0100

    Ajout des classes ExemplaireController, ExemplaireRepository, ExemplaireService et et ExemplaireServiceImpl.

---
+++ bibliotheque/src/main/java/org/ludo/bibliotheque/dao/ExemplaireRepository.java
@@ -0,0 +1,7 @@
+package org.ludo.bibliotheque.dao;
+
+import org.ludo.bibliotheque.entities.Exemplaire;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExemplaireRepository extends JpaRepository<Exemplaire, Long> {
+}
