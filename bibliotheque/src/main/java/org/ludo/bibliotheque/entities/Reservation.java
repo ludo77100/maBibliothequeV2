58fb7ba19311463cc171ed3ea89cc1f37b975b86
ludovic <ludovic>
2020-11-14 11:03:14 +0100

    Ajout de l'entité reservation avec la relation avec Livre

--- bibliotheque/src/main/java/org/ludo/bibliotheque/entities/Reservation.java
+++ bibliotheque/src/main/java/org/ludo/bibliotheque/entities/Reservation.java
@@ -1,21 +1,76 @@
 package org.ludo.bibliotheque.entities;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.io.Serializable;
+import java.util.Date;
 
 @Entity
-@Data
-@NoArgsConstructor
 public class Reservation implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long idReservation ;
-    //TODO créer l'entitié résérvation
+
+    private Date dateDemandeReservation ;
+
+    private String pseudoDemandeur ;
+
+    @JsonManagedReference
+    @ManyToOne
+    private Livre livre;
+
+    public Reservation() {
+        super();
+    }
+
+    public Reservation(long idReservation, Date dateDemandeReservation, String pseudoDemandeur, Livre livre) {
+        this.idReservation = idReservation;
+        this.dateDemandeReservation = dateDemandeReservation;
+        this.pseudoDemandeur = pseudoDemandeur;
+        this.livre = livre;
+    }
+
+    public long getIdReservation() {
+        return idReservation;
+    }
+
+    public void setIdReservation(long idReservation) {
+        this.idReservation = idReservation;
+    }
+
+    public Date getDateDemandeReservation() {
+        return dateDemandeReservation;
+    }
+
+    public void setDateDemandeReservation(Date dateDemandeReservation) {
+        this.dateDemandeReservation = dateDemandeReservation;
+    }
+
+    public String getPseudoDemandeur() {
+        return pseudoDemandeur;
+    }
+
+    public void setPseudoDemandeur(String pseudoDemandeur) {
+        this.pseudoDemandeur = pseudoDemandeur;
+    }
+
+    public Livre getLivre() {
+        return livre;
+    }
+
+    public void setLivre(Livre livre) {
+        this.livre = livre;
+    }
+
+    @Override
+    public String toString() {
+        return "Reservation{" +
+                "idReservation=" + idReservation +
+                ", dateDemandeReservation=" + dateDemandeReservation +
+                ", pseudoDemandeur='" + pseudoDemandeur + '\'' +
+                ", livre=" + livre +
+                '}';
+    }
 }
