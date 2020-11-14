5068fdf17d17464810ad483ecd54bb78c107e0a3
ludovic <ludovic>
2020-11-14 14:31:05 +0100

    Ajout d'une méthode permettant de vérifier la RG qui dicte qu'un utilisateur ne peut pas faire une réservation sur un livre qu'il a déjà en emprunt

--- bibliotheque/src/main/java/org/ludo/bibliotheque/service/service/impl/ReservationServiceImpl.java
+++ bibliotheque/src/main/java/org/ludo/bibliotheque/service/service/impl/ReservationServiceImpl.java
@@ -1,7 +1,9 @@
 package org.ludo.bibliotheque.service.service.impl;
 
+import org.ludo.bibliotheque.dao.EmpruntRepository;
 import org.ludo.bibliotheque.dao.LivreRepository;
 import org.ludo.bibliotheque.dao.ReservationRepository;
+import org.ludo.bibliotheque.entities.Emprunt;
 import org.ludo.bibliotheque.entities.Livre;
 import org.ludo.bibliotheque.entities.Reservation;
 import org.ludo.bibliotheque.service.ReservationService;
@@ -9,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
+import java.util.List;
 
 @Service
 public class ReservationServiceImpl implements ReservationService {
@@ -19,17 +22,41 @@ public class ReservationServiceImpl implements ReservationService {
     @Autowired
     ReservationRepository reservationRepository ;
 
+    @Autowired
+    EmpruntRepository empruntRepository ;
+
     @Override
     public Reservation ouvrirReservation(String pseudoDemandeur, String titreLivre) {
 
-        Livre livre = livreRepository.findByTitre(titreLivre);
+        Livre livreDemande = livreRepository.findByTitre(titreLivre);
+        List<Emprunt> listeEmpruntUtilisateur = empruntRepository.findAllByPseudoEmprunteurAndEnCoursIsTrue(pseudoDemandeur);
+
+        this.verifLivreReserveNonEmprunte(listeEmpruntUtilisateur, pseudoDemandeur);
+
         Date date = new Date() ;
         Reservation ouvrirReservation = new Reservation() ;
 
         ouvrirReservation.setDateDemandeReservation(date);
-        ouvrirReservation.setLivre(livre);
+        ouvrirReservation.setLivre(livreDemande);
         ouvrirReservation.setPseudoDemandeur(pseudoDemandeur);
 
         return reservationRepository.save(ouvrirReservation);
+    }
+
+    //TODO à tester
+    /**
+     * Cette méthode permet de vérifier qu'un utilisateur demandant une réservation pour un livre
+     * ne le possède pas déjà sur un emprunt
+     * @param listeEmpruntUtilisateur
+     * @param titreLivre
+     */
+    public void verifLivreReserveNonEmprunte(List<Emprunt> listeEmpruntUtilisateur, String titreLivre){
+
+        for (int i = 0 ; i < listeEmpruntUtilisateur.size() ; i++){
+            Emprunt emprunt = listeEmpruntUtilisateur.get(i);
+            String livre = emprunt.getExemplaire().getLivre().getTitre();
+            if (livre == titreLivre);
+            break;
+        }
     }
 }
